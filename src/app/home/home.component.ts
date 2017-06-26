d64594614cc6155353b3397dbfa7c56484760238
José de la Torre López <jose@MacBook-Pro-de-Jose-2.local>
2017-06-26 18:13:42 +0200

    Algunos ajustes con la seguridad. Servicio de ejemplo.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
+import { RopaService } from '../ropa.service'
 
 @Component({
 	selector: 'home',
-	templateUrl: 'home.component.html'
+	templateUrl: 'home.component.html',
+	providers: [RopaService]
 })
-export class HomeComponent{
+export class HomeComponent {
 	public title = "Página principal";
 	public nombre:string;
+	public prenda:string;
+	public color:string;
+	public prendas
+
+	constructor(private _ropaService: RopaService){
+		this.prendas = this._ropaService.list();
+	}
 
 	public guardarUsuario = function(event){
 		this.nombre = event.srcElement.value
 	}
+
+	public add = function() {
+		var nuevaPrenda = {id:0,prenda:"", color:""};
+		nuevaPrenda.prenda = this.prenda;
+		nuevaPrenda.color = this.color;
+		nuevaPrenda.id = this._ropaService.nextId();
+
+		this._ropaService.add(nuevaPrenda)
+	}
+
+	public delete = function(id){
+		this._ropaService.delete(id);
+	}
+
+
+
 }
---
+++ src/app/ropa.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class RopaService {
+
+	public nombre_prenda = 'Pantalones vaqueros'
+	public prendas = [
+		{"id":1,"prenda":"Pantalones", "color":"rojo"},
+		{"id":2,"prenda":"Camiseta", "color":"blanca"}
+	]
+
+  list(){
+  	return this.prendas
+  }
+
+  add(prenda){
+  		this.prendas.push(prenda)
+  }
+
+  nextId(){
+  	return this.prendas.length > 0 ? this.prendas[this.prendas.length-1].id+1 : 1;
+  }
+
+  delete(id){
+  	var elem = this.prendas.filter(function( obj ) {
+	  return obj.id == id;
+	});
+	if(elem.length > 0){
+		var index = this.prendas.indexOf(elem[0]);
+		this.prendas.splice(index,1);
+	}
+  }
+
+}
